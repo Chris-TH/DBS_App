2da0f6b03605fbc1dd65ad3af0d3d62e04fd9528
Chris <chris.letschert@gmx.de>
2021-06-05 22:06:06 +0200

    dbs

---
+++ app/sql/Data.sql
@@ -0,0 +1,5 @@
+CALL proc_userprofil_anlegen('Harvey', 'Specter', 'Harvey.Specter@gmail.com', 'PaulSchmidt123', 'M', '2000-05-10', 017156392);
+CALL proc_userprofil_anlegen('Donna', 'Paulsen', 'Donna.Paulsen@gmail.com', 'PaulSchmidt123', 'M', '2000-05-10', 017156392);
+CALL proc_userprofil_anlegen('Louis', 'Litt', 'Louis.Litt@gmail.com', 'PaulSchmidt123', 'M', '2000-05-10', 017156392);
+CALL proc_userprofil_anlegen('Mike', 'Ross', 'Mike.Ross@gmail.com', 'PaulSchmidt123', 'M', '2000-05-10', 017156392);
+CALL proc_userprofil_anlegen('Rachel', 'Zane', 'Rachel.Zane@gmail.com', 'PaulSchmidt123', 'M', '2000-05-10', 017156392);
